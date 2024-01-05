e2a065d32c840a4d1c122967bef555c841bf8c27
Devendra Royal <94845156+0xdev02@users.noreply.github.com>
2024-01-05 22:59:55 +0530

    Add files via upload
    Learnt Card applied CSS

---
+++ React_start/02_CardComponent/card.jsx
@@ -0,0 +1,12 @@
+function Card (){
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture" />
+            <h2>Devendra</h2>
+            <p>I'm learning MERN</p>
+        </div>
+
+    );
+}
+
+export default Card
