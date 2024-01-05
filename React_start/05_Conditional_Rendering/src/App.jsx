4c84fa48a25ae911de67332757b7981c213fa39c
Devendra Royal <94845156+0xdev02@users.noreply.github.com>
2024-01-06 00:58:36 +0530

    Create App.jsx

---
+++ React_start/05_Conditional_Rendering/src/App.jsx
@@ -0,0 +1,10 @@
+import UserGreeting from './UserGreeting.jsx'
+function App() {
+    return(
+      <>
+        <UserGreeting isLoggedIn={true} username="Devendra"/>
+      </>
+    );  
+}
+
+export default App
