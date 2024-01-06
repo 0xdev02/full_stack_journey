46a322593f172f7edc64011226a32a35cad9d955
Devendra Royal <94845156+0xdev02@users.noreply.github.com>
2024-01-07 01:12:41 +0530

    Create App.jsx
    onClick ondoubleClick
    "e" "event"

---
+++ React_start/07_ReactClickEvents/src/App.jsx
@@ -0,0 +1,11 @@
+import Button from './Button.jsx'
+import Profilepicture  from './Profilepic.jsx';
+function App() {
+    
+    return(<>
+          <Button/>
+          <Profilepicture/>
+        </>);  
+}
+
+export default App
